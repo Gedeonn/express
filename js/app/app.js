92fca1079dac0a1f7bbb0457908b4016d358bf57
gedeonn <gedeoniyonkuru@gmail.com>
2017-10-13 16:57:51 +0000

    done

--- js/app/app.js
+++ js/app/app.js
@@ -2,8 +2,8 @@
 // to add this function to the list of native javascript functions to allow javascript identify and execute it each time its called.
 // This is done by using the addEventListener() function.
 //
-document.addEventListener("deviceready", onDeviceReady, false);
-
+//document.addEventListener("deviceready", onDeviceReady, false);
+document.getElementById("button").addEventListener("click", onDeviceReady, false);
 
 //We decide to create a function to handle the 3rd party functions (eg. navigator.geolocation.getCurrentPosition)
 // which we earlier added to the native functions of the javascript
